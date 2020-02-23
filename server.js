6180ef2dc8433acae69bf7b00c2c885a1bb51810
Why-just-why <54150780+Why-just-why@users.noreply.github.com>
2020-02-23 18:22:00 +0530

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const ConnectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 //Connect to database
@@ -21,6 +22,10 @@ app.listen(PORT, err => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("API working");
-});
+if (process.env.NODE_ENV === "production") {
+  //Set Static
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
